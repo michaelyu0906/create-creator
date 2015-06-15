bc5198fc272f1fac597163b23b6e23d71cd0ddd1
michaelyu0906 <michael_yu0906@163.com>
2015-06-15 14:57:22 +0800

    struct

--- Creator/main.cpp
+++ Creator/main.cpp
@@ -1,4 +1,36 @@
-#include<stdio.h>
+#include<fstream>
+class homework
+{
+	public:
+		void print()
+		{ 
+
+		}
+};
+class subject
+{
+	public:
+		void print()
+		{
+
+		}
+};
+class chapter
+{
+	public:
+		void print()
+		{
+
+		}
+};
+class ques
+{
+	public:
+		void print()
+		{
+
+		}
+};
 int main()
 {
 	printf("test2\n");
