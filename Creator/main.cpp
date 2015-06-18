110e5b1e819b19594d984823e44a874050579464
michaelyu0906 <michael_yu0906@163.com>
2015-06-18 14:51:46 +0800

    subject-no(hash)

--- Creator/main.cpp
+++ Creator/main.cpp
@@ -1,77 +1,79 @@
 #include<fstream>
 #include<string>
+#include<map>
 using namespace std;
 ofstream fout("homework.xml");
 class homework
 {
-	private:
-		int subjectNum;
+private:
+	map <string, int> subjectNo;
+	int subjectNum = 0;
+public:
+	class subject
+	{
 	public:
-		void print()  //print homework
-		{ 
-			fout << "<homework>\n";
-			for (int i = 0; i < subjectNum; i++)
-				subjectHead[i]->print();
-			fout << "</homework>\n";
-		}
-		void Appendsubjects(int subject_Num)  //apply ram for subjects
+		string subjectName;
+		int chapterNum;
+		class chapter
 		{
-			subjectNum = subject_Num;
-			for (int i = 0; i < subjectNum; i++)
-				subjectHead[i] = new subject[1];
-		}
-		class subject
-		{	
+		private:
+			int queNum;
+			class ques
+			{
 			private:
-				string subjectName;
-				int chapterNum; 
-				class chapter
-				{
-					private:
-						int queNum;
-						class ques
-					{
-						private:
-							int queNo;
-							int difficulty;
-						public:
-						void print(int chapterNo)
-						{
-							fout << "\t\t\t<ques>\n";
-							fout << "\t\t\t\t<quesNo>" << chapterNo << "." << queNo << "</quesNo>\n";
-							fout << "\t\t\t\t<difficulty>" << difficulty << "</difficulty>\n";
-						}
-					};
-						ques __ques[2];
-					public:
-						int chapterNo;
-					void print()
-					{
-						fout << "\t\t<chapter No=\"" << chapterNo << "\">\n";
-						for (int i = 0; i < queNum; i++)
-							__ques[i].print(chapterNo);
-						fout << "\t\t</chapter>\n";
-					}
-				};
-				chapter* chapterHead[100];
+				int queNo;
+				int difficulty;
 			public:
-				void subjectDetails(string subject_Name, int chapter_Num)  //apply ram for chapters
+				void print(int chapterNo)
 				{
-					subjectName = subject_Name;
-					chapterNum = chapter_Num;
-					for (int i = 0; i < chapterNum; i++)
-						chapterHead[i] = new chapter[1];
+					fout << "\t\t\t<ques>\n";
+					fout << "\t\t\t\t<quesNo>" << chapterNo << "." << queNo << "</quesNo>\n";
+					fout << "\t\t\t\t<difficulty>" << difficulty << "</difficulty>\n";
 				}
-				void print() //print a subject
-				{
-					fout << "\t<subject name=\"" << subjectName << "\">";
-					for (int i = 0; i < chapterNum; i++)
-						chapterHead[i]->print();
-					fout << "\t</subject>\n";
-				}
+			};
+			ques __ques[2];
+		public:
+			int chapterNo;
+			void print()
+			{
+				fout << "\t\t<chapter No=\"" << chapterNo << "\">\n";
+				for (int i = 0; i < queNum; i++)
+					__ques[i].print(chapterNo);
+				fout << "\t\t</chapter>\n";
+			}
 		};
-	private:
-		subject* subjectHead[100];//point the head to subject
+		chapter* chapterHead[100];
+	public:
+		void chapterAppend(int chapter_Num)
+		{
+			chapterNum = chapter_Num;
+			for (int i = 0; i < chapterNum; i++)
+				chapterHead[i] = new chapter[1];
+		}
+		void print() //print a subject
+		{
+			fout << "\t<subject name=\"" << subjectName << "\">";
+			for (int i = 0; i < chapterNum; i++)
+				chapterHead[i]->print();
+			fout << "\t</subject>\n";
+		}
+	};
+	void print()  //print homework
+	{
+		fout << "<homework>\n";
+		for (int i = 0; i < subjectNum; i++)
+			subjectHead[i]->print();
+		fout << "</homework>\n";
+	}
+	void subjectAppend(string subject_Name, int chapter_Num)  //apply ram for a subject
+	{
+		subjectHead[subjectNum++] = new subject[1];
+		subjectHead[subjectNum - 1]->subjectName = subject_Name;
+		subjectHead[subjectNum - 1]->chapterAppend(chapter_Num);
+	}
+
+private:
+	subject* subjectHead[100];//point the head to subject
 };
 
 int main()
